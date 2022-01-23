c3d35fdb7516cd84b49a53affa85a5f367162116
Ben <bgrowcott@hotmail.co.uk>
2022-01-23 21:54:39 +0000

    just some little changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -21,10 +21,11 @@ setInterval(updateTime, 1000);
 function coordSearch() {
   fetch(weatherApiUrl)
     .then(function (response) {
-      if (response.status === 200){
+      if (response.status === 200) {
         return response.json();
+      } else {
+        throw response;
       }
-      else {throw response}
     })
     .then(function (data) {
       coordData = data;
@@ -33,17 +34,17 @@ function coordSearch() {
       oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=8d0c1a27ce3e47ce06696db25a3b205f`;
     })
     .then(weatherSearch)
-    .catch(errorBehaviour)
+    .catch(errorBehaviour);
 }
 
-function errorBehaviour(){
+function errorBehaviour() {
   //show modal
-  $('#errorModal').modal()
+  $("#errorModal").modal();
   // remove bad search from local storage
-  localStorageArray.pop()
-  localStorage.setItem("history", JSON.stringify(localStorageArray))
+  localStorageArray.pop();
+  localStorage.setItem("history", JSON.stringify(localStorageArray));
   // remove bad search from recent searches
-  $('#searchHistory').children().eq(0).remove()
+  $("#searchHistory").children().eq(0).remove();
 }
 
 // function for getting weather at coordinates and call render functions
@@ -57,7 +58,7 @@ function weatherSearch() {
     })
     .then(renderCurrentWeather)
     .then(renderForecast)
-    .then(setDayNight)
+    .then(setDayNight);
 }
 // load default of Birmingham
 coordSearch();
@@ -65,7 +66,9 @@ coordSearch();
 // getting city name from user input and call coordSearch
 function citySearch(e) {
   e.preventDefault();
-  if ($("#cityInput").val() === ''){return}
+  if ($("#cityInput").val() === "") {
+    return;
+  }
   for (let i = 1; i < forecastCardArray.length; i++) {
     $(forecastCardArray[i]).empty();
   }
@@ -89,7 +92,7 @@ function renderCurrentWeather() {
   );
   $("#weatherIcon").attr(
     "src",
-    `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
   );
   $("#temp").text(`Temperature: ${weatherData.current.temp} °C`);
   $("#wind").text(`Wind speed: ${weatherData.current.wind_speed} meter/sec`);
@@ -97,6 +100,7 @@ function renderCurrentWeather() {
   $("#UV").text(` ${weatherData.current.uvi} `);
   if (weatherData.current.uvi > 2) {
     $("#UV").css("background-color", "yellow");
+    $("#UV").css("color", "black");
   } else if (weatherData.current.uvi > 5) {
     $("#UV").css("background-color", "red");
   } else $("#UV").css("background-color", "green");
@@ -135,14 +139,16 @@ function renderForecast() {
 let historyArray = [];
 function history() {
   historyArray = JSON.parse(localStorage.getItem("history"));
-  // don't add it again if it's already there  
-  for (let i = 0; i < historyArray.length; i++){
+  // don't add it again if it's already there
+  for (let i = 0; i < historyArray.length; i++) {
     if ($("#cityInput").val().toUpperCase() === historyArray[i]) {
       return;
     }
   }
   // limit history size
-  if (historyArray.length > 10){historyArray.shift()}
+  if (historyArray.length > 10) {
+    historyArray.shift();
+  }
 
   historyArray.push($("#cityInput").val().toUpperCase());
   localStorage.setItem("history", JSON.stringify(historyArray));
@@ -152,7 +158,7 @@ function history() {
 let localStorageArray = JSON.parse(localStorage.getItem("history"));
 renderHistory();
 function renderHistory() {
-  localStorageArray = JSON.parse(localStorage.getItem("history"))
+  localStorageArray = JSON.parse(localStorage.getItem("history"));
   for (let i = 0; i < localStorageArray.length; i++) {
     $("#searchHistory").prepend(
       $(`<li>${localStorageArray[i].toUpperCase()}</li>`)
@@ -162,14 +168,15 @@ function renderHistory() {
 
 //add latest input
 function addSearch() {
-  // don't add it again if it's already there  
-  if (localStorageArray.length > 0){
-  for (let i = 0; i < localStorageArray.length; i++) {
-    if ($("#cityInput").val().toUpperCase() === localStorageArray[i]) {
-      return;
+  // don't add it again if it's already there
+  if (localStorageArray.length > 0) {
+    for (let i = 0; i < localStorageArray.length; i++) {
+      if ($("#cityInput").val().toUpperCase() === localStorageArray[i]) {
+        return;
+      }
     }
-  }}
-  localStorageArray = JSON.parse(localStorage.getItem("history"))
+  }
+  localStorageArray = JSON.parse(localStorage.getItem("history"));
   // add to list
   $("#searchHistory").prepend(
     $(`<li>${$("#cityInput").val().toUpperCase()}</li>`)
@@ -186,14 +193,14 @@ function searchFromHistory(e) {
 // background day/night theme
 
 function setDayNight() {
-  if (moment().unix() + weatherData.timezone_offset > weatherData.current.sunset){
-    $('body').css('background-image', 'url(assets/images/nightsky.jpg)')
-    $('body').css('color', 'white')
-  }
-  else {
-    $('body').css('background-image', 'url(assets/images/pikrepo.com.jpg)')
-    $('body').css('color', 'black')
+  if (
+    moment().unix() + weatherData.timezone_offset >
+    weatherData.current.sunset
+  ) {
+    $("body").css("background-image", "url(assets/images/nightsky.jpg)");
+    $("body").css("color", "white");
+  } else {
+    $("body").css("background-image", "url(assets/images/pikrepo.com.jpg)");
+    $("body").css("color", "black");
   }
 }
-
-
